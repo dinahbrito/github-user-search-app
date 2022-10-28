9393cff9cfacc79633b1396dfe283cc594bbf6f3
Brito <dibrito@teksystems.com>
2022-10-28 10:21:29 -0700

    Added theme toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
 import {Icon} from "./components/icon"
+import {useState} from 'react';
+import { useEffect } from 'react';
 
 function App() {
+const [theme, setTheme] = useState(
+  localStorage.getItem('theme') || 'dark'
+  );
+const toggleTheme = () => {
+  if (theme === 'dark') {
+    setTheme('light');
+  } else {
+    setTheme('dark');
+  }
+};
+
+useEffect(() => {
+  localStorage.setItem('theme', theme);
+  document.body.className = theme;
+}, [theme]);
+
   return (
-    <main className="App">
+    <main className={`App ${theme}`}>
       <header>
         <h1>devfinder</h1>
-        <button className='theme-toggle'>Light 
+        <button className='theme-toggle' onClick={toggleTheme}>Light 
         <Icon name='sun'/>
         </button>
       </header>
 
-      <div className='search-bar'>
-        <input type="search" placeholder="Search GitHub username.." />
-        <button type="submit">Search</button>
-      </div>
-
-    <div>
-      stuff
-    </div>
     
     </main>
   );
